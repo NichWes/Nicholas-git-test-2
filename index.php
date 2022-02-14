6a9758069b92c9f1b0232991bc32e21118d8e2e6
NichWes <nicholaswestley06@gmail.com>
2022-02-14 21:38:16 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi praesentium neque ipsa corrupti dolore corporis accusantium ipsam voluptates beatae et modi eum, aliquam vel libero eaque, fugiat, necessitatibus alias!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
